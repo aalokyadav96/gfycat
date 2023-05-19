770b021bf663dd7837c93650657f74dbcdd70658
Aalok Yadav <35891906+aalokyadav96@users.noreply.github.com>
2023-05-19 18:03:29 +0530

    Risky Business
    Rollback if needed

--- main.go
+++ main.go
@@ -6,220 +6,186 @@ import (
 	"html/template"
 	"io/ioutil"
 	"log"
-	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
-const maxUploadSize = 2 * 1024 * 1024 // 2 mb
+const PORT = "4000"
+const maxUploadSize = 10 * 1024 * 1024 // 8 mb
 var uploadPath = "./uploads"
-//var tmpl = template.Must(template.ParseGlob("templates/*.html"))
+var posterpath =  "./poster"
 var tmpl = template.Must(template.ParseGlob("_includes/*.html"))
 
+type Res struct {
+	Video string
+	Poster string
+}
 
 func main() {
 	http.HandleFunc("/", uploadFileHandler())
-	http.HandleFunc("/headless", headlessUpload())
-	http.HandleFunc("/delete", deleteUploads)
+	http.HandleFunc("/v/", viewPost())
+	http.HandleFunc("/getall", GetAll())
+	http.HandleFunc("/del/", DeleteVid())
 
-	fs := http.FileServer(http.Dir(uploadPath))
-	http.Handle("/files/", http.StripPrefix("/files", fs))
+	vidfs := http.FileServer(http.Dir(uploadPath))
+	http.Handle("/files/", http.StripPrefix("/files", vidfs))
 
-	thumfs := http.FileServer(http.Dir("./thumbs"))
-	http.Handle("/img/", http.StripPrefix("/img", thumfs))
+	posterfs := http.FileServer(http.Dir(posterpath))
+	http.Handle("/poster/", http.StripPrefix("/poster", posterfs))
 
-	log.Print("Server started on :4000")
-	log.Fatal(http.ListenAndServe(":4000", nil))
+	log.Print("Server started on localhost:4000")
+	log.Fatal(http.ListenAndServe(GetPort(), nil))
+}
+
+ func GetPort() string {
+ 	var port = os.Getenv("PORT")
+ 	var env = os.Getenv("ENV")
+ 	if port == "" {
+ 		port = "4000"
+ 		fmt.Println("INFO: defaulting to " + port)
+ 	}
+ 	if env == "" {
+		return "localhost:" + port
+ 	} else {
+		return ":" + port
+	}
+}
+
+func viewPost()  http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			title := r.URL.Path[len("/v/"):]
+			fmt.Println("path", r.URL.Path)
+			res := Res {Video: "/files/"+ title, Poster: "/poster/"+ title}
+			tmpl.ExecuteTemplate(w, "head.html", nil)
+			tmpl.ExecuteTemplate(w, "viewpost.html", res)
+			return
+		}
+	})
+}
+
+func GetAll()  http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			fmt.Println("path", r.URL.Path)
+			entries, err := os.ReadDir("./uploads")
+			if err != nil {
+				log.Fatal(err)
+			}
+		var vids []string
+			for _, e := range entries {
+				vids = append(vids, e.Name())
+			}
+			tmpl.ExecuteTemplate(w, "head.html", nil)
+			tmpl.ExecuteTemplate(w, "allfiles.html", vids)
+			return
+		}
+	})
+}
+
+func DeleteVid()  http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			title := r.URL.Path[len("/del/"):]
+			err := os.Remove("./uploads/"+title+".mp4")  // remove a single file
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("path", r.URL.Path)
+			w.Write([]byte("done"))
+			return
+		}
+	})
 }
 
 func uploadFileHandler() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
-			tmpl.ExecuteTemplate(w, "head.html",nil)
-			tmpl.ExecuteTemplate(w,"index.html",nil)
+			fmt.Println("path", r.URL.Path)
+			tmpl.ExecuteTemplate(w, "head.html", nil)
+			tmpl.ExecuteTemplate(w, "index.html", nil)
 			return
 		}
-		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
-			fmt.Printf("Could not parse multipart form: %v\n", err)
-			renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
-			return
-		}
-var pics []string
-		files := r.MultipartForm.File["imgfile"]
-	for _, fileHeader := range files {
-		file, err := fileHeader.Open()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		defer file.Close()
-
-		// Get and print out file size
-		fileSize := fileHeader.Size
-		fmt.Printf("File size (bytes): %v\n", fileSize)
-		// validate file size
-		if fileSize > maxUploadSize {
-			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
-			return
-		}
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			renderError(w, "INVALID_FILE", http.StatusBadRequest)
-			return
-		}
-
-		// check file type, detectcontenttype only needs the first 512 bytes
-		detectedFileType := http.DetectContentType(fileBytes)
-		switch detectedFileType {
-		case "video/mp4", "video/webm":
-		case "image/gif", "image/png", "image/jpg", "image/jpeg":
-			break
-		default:
-			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-			return
-		}
-		fileName := randToken(12)
-		fileEndings, err := mime.ExtensionsByType(detectedFileType)
-		if err != nil {
-			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-			return
-		}
-	if fileEndings[0] == ".f4v" {fileEndings[0] = ".mp4"}
-		newFileName := fileName + fileEndings[0]
-
-		newPath := filepath.Join(uploadPath, newFileName)
-		fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
-
-		// write file
-		newFile, err := os.Create(newPath)
-		if err != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		defer newFile.Close() // idempotent, okay to call twice
-		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		
-
-		pics = append(pics,"/files/"+newFileName)
-		}
-		
-	fmt.Println(pics)
-	tmpl.ExecuteTemplate(w,"returnUploads.html", pics)
-
-//		w.Write([]byte(fmt.Sprintf("SUCCESS - use /files/%v to access the file", newFileName)))
-//			t, _ := template.ParseFiles("templates/returnUploads.html")
-//			t, _ := template.ParseFiles("res.html")
-//			t.Execute(w, "files/"+newFileName)
-//			return
-	})
-}
-
-func headlessUpload() http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method != "POST" {
-//			w.Write([]byte(fmt.Sprintf("only post is allowed")))
-//			return
-//		}
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
+			fmt.Println("path", r.URL.Path)
 		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
 			fmt.Printf("Could not parse multipart form: %v\n", err)
 			renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
 			return
 		}
 		var fileEndings string
+		var fileName string
 		var pics []string
 		files := r.MultipartForm.File["imgfile"]
-	for _, fileHeader := range files {
-		file, err := fileHeader.Open()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		for _, fileHeader := range files {
+			file, err := fileHeader.Open()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 
-		defer file.Close()
+			defer file.Close()
 
-		// Get and print out file size
-		fileSize := fileHeader.Size
-		fmt.Printf("File size (bytes): %v\n", fileSize)
-		// validate file size
-		if fileSize > maxUploadSize {
-			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
-			return
-		}
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			renderError(w, "INVALID_FILE", http.StatusBadRequest)
-			return
-		}
+			// Get and print out file size
+			fileSize := fileHeader.Size
+			fmt.Printf("File size (bytes): %v\n", fileSize)
+			// validate file size
+			if fileSize > maxUploadSize {
+				renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+				return
+			}
+			fileBytes, err := ioutil.ReadAll(file)
+			if err != nil {
+				renderError(w, "INVALID_FILE"+http.DetectContentType(fileBytes), http.StatusBadRequest)
+				return
+			}
 
-		// check file type, detectcontenttype only needs the first 512 bytes
-		detectedFileType := http.DetectContentType(fileBytes)
-		switch detectedFileType {
-		case "video/mp4":
-			fileEndings = ".mp4"
-			break
-		case "video/webm":
-			fileEndings = ".webm"
-			break
-		case "image/gif":
-			fileEndings = ".gif"
-			break
-		case "image/png":
-			fileEndings = ".png"
-			break
-		case "image/jpg", "image/jpeg":
-			fileEndings = ".jpg"
-			break
-		default:
-			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-			return
-		}
-		fileName := randToken(12)
-//		fileEndings, err := mime.ExtensionsByType(detectedFileType)
+			// check file type, detectcontenttype only needs the first 512 bytes
+			detectedFileType := http.DetectContentType(fileBytes)
+			switch detectedFileType {
+			case "video/mp4":
+				fileEndings = ".mp4"
+				break
+			case "video/webm":
+				fileEndings = ".webm"
+				break
+			case "image/gif":
+				fileEndings = ".gif"
+				break
+			default:
+				renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+				return
+			}
+			fileName = randToken(12)
+			//		fileEndings, err := mime.ExtensionsByType(detectedFileType)
 
-		if err != nil {
-			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-			return
-		}
-		fmt.Println(fileEndings)
-//		newFileName := fileName + fileEndings[0]
-		newFileName := fileName + fileEndings
-		newPath := filepath.Join(uploadPath, newFileName)
-		fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
+			if err != nil {
+				renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
+				return
+			}
+			newFileName := fileName + fileEndings
 
-		// write file
-		newFile, err := os.Create(newPath)
-		if err != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		defer newFile.Close() // idempotent, okay to call twice
-		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		
+			newPath := filepath.Join(uploadPath, newFileName)
+			fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
 
-		pics = append(pics,"/files/"+newFileName)
-		}
-		
-	fmt.Println(pics)
-//	tmpl.ExecuteTemplate(w,"returnUploads.html", )
+			// write file
+			newFile, err := os.Create(newPath)
+			if err != nil {
+				renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
+			defer newFile.Close() // idempotent, okay to call twice
+			if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
+				renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+				return
+			}
 
-		w.Write([]byte(fmt.Sprintf("SUCCESS : ", pics)))
-//			t, _ := template.ParseFiles("templates/returnUploads.html")
-//			t, _ := template.ParseFiles("res.html")
-//			t.Execute(w, "files/"+newFileName)
-//			return
+			pics = append(pics, "/files/"+newFileName)
+		}
+		fmt.Println(pics)
+	w.Write([]byte("/v/"+fileName))
+//		tmpl.ExecuteTemplate(w, "player.html", pics)
+
 	})
 }
 
@@ -232,14 +198,4 @@ func randToken(len int) string {
 	b := make([]byte, len)
 	rand.Read(b)
 	return fmt.Sprintf("%x", b)
-}
-
-func deleteUploads(w http.ResponseWriter, r *http.Request) {
-   err := os.Remove(uploadPath)
-   if err != nil {
-      fmt.Println(err)
-   } else {
-      fmt.Println("Directory", uploadPath, "removed successfully")
-   }
-	w.Write([]byte(fmt.Sprintf("deleted all")))
 }
